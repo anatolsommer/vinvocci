6ccb883bcebbae3e9745eb6193b6c564e352b1a3
anatolsommer <anatol@anatol.at>
2016-01-03 17:59:03 +0100

    added tests, v0.0.3

--- History.md
+++ History.md
@@ -1,8 +1,13 @@
+0.0.3 / 2016-01-03
+==================
+
+  * added tests
+
 0.0.2-beta / 2016-01-02
 =======================
 
   * appending when logging to file
-  * added LICENCE
+  * added LICENSE
 
 0.0.1-beta / 2015-31-12
 =======================
---
+++ test/test.js
@@ -0,0 +1,111 @@
+var assert=require('assert'), fs=require('fs'),  path=require('path'),
+  Log=require('..'), file=__dirname+'/tmp'+Date.now()+'.log';
+
+describe('Vinvocci', function() {
+  var log=new Log(), res;
+
+  it('should log to stdout', function() {
+    res=getOutput(function() {
+      log.info('test');
+    });
+    assert.equal(res[0].msg, 'test');
+  });
+
+  it('should log time', function() {
+    assert(res[0].time);
+  });
+
+  it('should log INFO and ERROR', function() {
+    res=getOutput(function() {
+      log.info('test');
+      log.error('test');
+      log.debug('nope');
+    });
+    assert.equal(res[0].msg, 'test');
+    assert.equal(res[0].type, 'INFO');
+    assert.equal(res[1].type, 'ERROR');
+  });
+
+  it('should supress DEBUG', function() {
+    assert.equal(res[2], undefined);
+  });
+
+  it('should accept string as log level', function() {
+    log.setLevel('eRRor');
+    res=getOutput(function() {
+      log.info('test1');
+      log.error('test2');
+    });
+    assert.equal(res[0].msg, 'test2');
+    assert.equal(res[0].type, 'ERROR');
+    assert.equal(res[1], undefined);
+  });
+
+  it('should set log level to DEBUG', function() {
+    log.setLevel(Log.DEBUG);
+    res=getOutput(function() {
+      log.debug('test');
+    });
+    assert.equal(res[0].msg, 'test');
+    assert.equal(res[0].type, 'DEBUG');
+  });
+
+  it('should log component when set', function() {
+    res=getOutput(function() {
+      log.debug('test1');
+      log.component='compo';
+      log.info('test2');
+    });
+    assert.equal(res[0].component, undefined);
+    assert.equal(res[1].component, 'compo');
+  });
+
+  it('should change component property name', function() {
+    res=getOutput(function() {
+      log.componentProp='app';
+      log.debug('test1');
+    });
+    assert.equal(res[0].component, undefined);
+    assert.equal(res[0].app, 'compo');
+  });
+
+  it('should log to file', function(done) {
+    log=new Log(null, null, file);
+    log.info('test1');
+    setTimeout(function() {
+      res=convertToArray(fs.readFileSync(file));
+      assert.equal(res[0].msg, 'test1');
+      done();
+    }, 30);
+  });
+
+  it('should append to file', function(done) {
+    log=new Log(null, null, file);
+    log.info('test2');
+    setTimeout(function() {
+      res=convertToArray(fs.readFileSync(file));
+      assert.equal(res[0].msg, 'test1');
+      assert.equal(res[1].msg, 'test2');
+      done();
+    }, 30);
+  });
+
+  it('should remove test log file', function() {
+    fs.unlinkSync(file);
+  });
+
+});
+
+function getOutput(cb) {
+  var write=process.stdout.write, buf='';
+  process.stdout.write=function(data) {
+    buf+=data;
+  };
+  cb();
+  process.stdout.write=write;
+  return convertToArray(buf);
+}
+
+function convertToArray(str) {
+  return JSON.parse('['+str.toString().trim().replace(/\n/g, ',')+']');
+}
